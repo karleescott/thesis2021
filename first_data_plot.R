bfc869a9fed02cf07fcaf5b01f4837cdd2427fec
karlee <karlee.scott@westpoint.edu>
2020-09-04 15:46:16 +0000

    more plots

--- first_data_plot.R
+++ first_data_plot.R
@@ -1,11 +1,13 @@
 library(tidyverse)
-data <- read.csv("/data/ADSB/Opensky/states_2020-07-13-00.csv")
+data <- read.csv("/data/ADSB/OpenSky/states_2020-07-13-00.csv")
 data <- na.omit(data)
 View(data)
 data1 <- data %>%
   filter(icao24 == "00d170")
 data2 <- data %>%
-  filter(icao24 == "e80450" || icao24 == "00d170")
+  filter(icao24 == "e80450")
+data3 <- data %>%
+  filter(icao24 == "e495fc")
 
 
 
@@ -18,7 +20,7 @@ col_scale <- function(palette = "Spectral", name = "", limits = NULL) {
                          limits = limits)
 }
 
-plot <- ggplot(data1) + # plot points
+plot1 <- ggplot(data1) + # plot points
   geom_point(aes(x = lon,y = lat, # lon and lat
                  colour = time), # attribute color
              size = 2) + # make all points larger
@@ -28,5 +30,28 @@ plot <- ggplot(data1) + # plot points
   coord_fixed(xlim = c(152.7, 153.2),
               ylim = c(-26.5, -27)) + # zoom in
   theme_bw() # B&W theme
-plot
+plot1
 
+plot2 <- ggplot(data2) + # plot points
+  geom_point(aes(x = lon,y = lat, # lon and lat
+                 colour = time), # attribute color
+             size = 2) + # make all points larger
+  col_scale(name = "time") + # attach color scale
+  xlab("Longitude (deg)") + # x-axis label
+  ylab("Latitude (deg)") + # y-axis label
+  coord_fixed(xlim = c(174.2, 174.6),
+              ylim = c(-35.0, -35.5)) + # zoom in
+  theme_bw() # B&W theme
+plot2
+
+plot3 <- ggplot(data3) + # plot points
+  geom_point(aes(x = lon,y = lat, # lon and lat
+                 colour = time), # attribute color
+             size = 2) + # make all points larger
+  col_scale(name = "time") + # attach color scale
+  xlab("Longitude (deg)") + # x-axis label
+  ylab("Latitude (deg)") + # y-axis label
+  coord_fixed(xlim = c(-47.1, -47.6),
+              ylim = c(-22.9, -23.4)) + # zoom in
+  theme_bw() # B&W theme
+plot3
