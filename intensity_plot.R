6a53230f0f203b5ca568722a24ee4304675b8551
karlee <>
2020-09-11 18:34:45 +0000

    density

--- intensity_plot.R
+++ intensity_plot.R
@@ -1,13 +1,25 @@
 library(tidyverse)
-
 usdata <- read.csv("/lfs/karlee_combined_data.csv")
 usdata1 <- usdata %>%
   filter(time == 1594598410)
 
+x <- as.vector(usdata1$lon)
+y <- as.vector(usdata1$lat)
+P <- ppp(x, y, c(-125, -65), c(25, 50), unitname=c("degrees","degrees"))
+P
+summary(P)
+plot(P)
+#bw.nrd0 implements a rule-of-thumb for choosing the bandwidth of a Gaussian kernel density estimator. 
+#It defaults to 0.9 times the minimum of the standard deviation and the interquartile range divided by 
+#1.34 times the sample size to the negative one-fifth power (= Silverman's ‘rule of thumb’, Silverman 
+#(1986, page 48, eqn (3.31))) unless the quartiles coincide when a positive result will be guaranteed.
+plot(density(P, bw = "nrd0", kernel = "gaussian"), main = "Intensity Plot", xlab = "Longitude (degrees)", ylab = "Latitude (degrees)")
+
 #https://www.r-graph-gallery.com/2d-density-plot-with-ggplot2.html
-intplot1 <- ggplot(usdata1, aes(x=lon, y=lat) ) +
+intplot1 <- ggplot(usdata1, aes(x=lon, y=lat)) +
+#default gaussian with bandwidth.nrd(x) "rule of thumb"   
   stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE) +
-  scale_fill_distiller(palette= "Spectral", direction=1) +
+  scale_fill_distiller(palette= "Spectral", direction=-1) +
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0)) +
   xlab("Longitude (deg)") + # x-axis label
@@ -21,7 +33,7 @@ intplot1
 
 combinedplot <- ggplot(usdata1, aes(x=lon, y=lat) ) +
   stat_density_2d(aes(fill = ..density..), geom = "raster", contour = FALSE) +
-  scale_fill_distiller(palette= "Spectral", direction=1) +
+  scale_fill_distiller(palette= "Spectral", direction=-1) +
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0)) +
   xlab("Longitude (deg)") + # x-axis label
