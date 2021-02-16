75d9cbe173130a867019f2c8545fcbc8d5b1eadd
karlee <>
2021-02-16 19:16:21 +0000

    arrival update

--- make_Data.R
+++ make_Data.R
@@ -192,6 +192,190 @@ makeData <- function(lat,lon,startTime){
   return(data)
 }
 
+makeData2 <- function(lat,lon,startTime){ 
+  #lat <- 35.8801
+  #lon <- -78.7880
+  #startTime <- 0
+  print(startTime)
+  data <- usdata
+  data <- data %>%
+    filter(time >= (1594598400+3600*6*(startTime)) & time < (1594598400+3600*6*(startTime+1)))
+  numclusters = 4
+  #filter data
+  data <- na.omit(data)
+  data <- data %>%
+    filter(lon >= -125 & lon <= -65 & lat >= 25 & lat <= 50)
+  data <- cbind(data$time,as.character(data$icao24),data$lon,data$lat,as.character(data$onground))
+  data <- data.frame(data)
+  colnames(data) <- c("time", "icao24", "lon", "lat","onground")
+  
+  #distance from RDU
+  distance <- c()
+  for(row in 1:nrow(data)){
+    distance[row] <- sqrt((as.numeric(as.character(data[row,3]))-(lon))^2 + (as.numeric(as.character(data[row,4]))-(lat))^2)
+  }
+  
+  data <- cbind(data, distance)
+  
+  #find ids that pass through RDU (1 degree buffer)
+  list = c()
+  i = 1
+  for (row in 1:nrow(data)){
+    if(data[row,6] <= 1) {
+      list[i] <- as.character(data[row,2])
+      i <- i + 1
+    }
+  }
+  
+  #remove repeat ids, filter data to paths within 1 degree of RDU
+  res <- c()
+  i = 1
+  for (j in list){ 
+    if (j %in% res){ 
+    }
+    else{
+      res[i] <- j
+      i <- i + 1
+    }
+  }
+  
+  res <- sort(res)
+  data <- data %>%
+    filter(icao24 %in% res)
+  
+  #find start time based on smallest distance from RDU
+  start <- data.frame()
+  i = 1
+  for(j in res){
+    data1 <- data %>%
+      filter(icao24 == j)
+    mindistance <- min(data1$distance)
+    data1 <- data1 %>%
+      filter(distance == mindistance) %>%
+      arrange(time)
+    start[i,1] <- j
+    start[i,2] <- data1[1,1]
+    i <- i + 1
+  }
+  
+  #add start time to data
+  data <- data %>%
+    arrange(icao24)
+  start <- start %>%
+    arrange(V1)
+  
+  st <- c()
+  i = 1
+  for(r in 1:nrow(data)){
+    if(as.character(data[r,2]) == as.character(start[i,1])){
+      st[r] <- as.numeric(as.character(start[i,2]))
+    }
+    else{
+      i <- i + 1
+      st[r] <- as.numeric(as.character(start[i,2]))
+    }
+  }
+  
+  data <- cbind(data, st)
+  
+  #remove all times after start time
+  #if the plane takes more than one route through the area, this approach might delete previous routes
+  data[,1] <- as.numeric(as.character(data[,1]))
+  data <- data %>%
+    filter(time<=st)
+  
+  #change times to start at 1
+  data <- data %>%
+    arrange(icao24, time)
+  
+  tz <- c(1)
+  i = 1
+  for(r in 2:nrow(data)){
+    if(as.character(data[r-1,2]) == as.character(data[r,2])){
+      i <- i + 1
+      tz[r] <- i
+    }
+    else{
+      i = 1
+      tz[r] <- i
+    }
+  }
+  data <- cbind(data, tz)
+  
+  tz_count <- table(data$tz)
+  tz_count <- as.data.frame(tz_count)
+  tz_count <- tz_count %>%
+    filter(Freq < 5)
+  cap <- as.numeric(as.character(tz_count[1,1]))
+  data <- data %>%
+    filter(tz < cap)
+  
+  #assign initial random group
+  groups <- data.frame()
+  l = 1
+  for (n in 1:length(res)){
+    groups[n,1] <- res[n]
+    groups[n,2] <- l
+    if(l == numclusters){
+      l <- 1
+    }
+    else{
+      l <- l + 1
+    }
+  }
+  colnames(groups) = c("res", "group")
+  data <- data %>%
+    arrange(icao24)
+  fundata <- groups %>%
+    arrange(res)
+  
+  group <- c()
+  i = 1
+  for(r in 1:nrow(data)){
+    if(as.character(data[r,2]) == as.character(fundata[i,1])){
+      group[r] <- fundata[i,2]
+    }
+    else{
+      i <- i + 1
+      group[r] <- fundata[i,2]
+    }
+  }
+  
+  data <- cbind(data, group)
+  data[,2] <- as.character(data[,2])
+  data[,3] <- as.numeric(as.character(data[,3]))
+  data[,4] <- as.numeric(as.character(data[,4]))
+  
+  #remove data that is a second flight form same icao24 (aka lands and then re take's off)
+  grounddata <- data
+  
+  grounddata_length <- nrow(grounddata)
+  remove_list <- c()
+  i <- 1
+  while(i <= grounddata_length){
+    id_name <- grounddata[i,2]
+    if(grounddata[i,5] == "True" & grounddata[i,6] > 1){
+      remove_list <- append(i,remove_list)
+      i <- i + 1
+      id_name1 <- grounddata[i,2]
+      while(id_name == id_name1){
+        remove_list <- append(i,remove_list)
+        id_name <- grounddata[i,2]
+        i <- i + 1
+        id_name1 <- grounddata[i,2]
+      }
+    }
+    i <- i + 1
+  }
+  
+  if(is.null(remove_list) == "FALSE"){  
+    data <- grounddata[-(remove_list),]
+  }
+  data <- data[,-5]
+  
+  return(data)
+}
+
 makeCluster <- function(data) {
   numclusters <- length(unique(data$group))
   groups <- sort(unique(data$group))
@@ -398,8 +582,12 @@ compareMean <- function(fun1, fun2, threshold){
 }
 
 #all specified airport data for 13 July. Must be filtered by hour to use. 
-combineData <- function(lat,lon,threshold){
-  data <- makeData(lat,lon,0)
+combineData <- function(lat,lon,arrive_depart,threshold){
+  if(arrive_depart == "depart"){
+    data <- makeData(lat,lon,0)}
+  else{
+    data <- makeData2(lat,lon,0)
+  }
   everything <- makeCluster(data)
   everything1 <- makeCluster(data.frame(everything[1]))
   fun1 <- data.frame(everything[2])
@@ -415,7 +603,11 @@ combineData <- function(lat,lon,threshold){
   data <- cbind(as.data.frame(everything1[2]),time_of_day = 0)
   
   for (i in 1:3){
-    data1 <- makeData(lat,lon,i)
+    if(arrive_depart == "depart"){
+      data1 <- makeData(lat,lon,i)}
+    else{
+      data1 <- makeData2(lat,lon,i)
+    }
     everything <- makeCluster(data1)
     everything2 <- makeCluster(data.frame(everything[1]))
     fun1 <- data.frame(everything[2])
@@ -554,6 +746,7 @@ totalFunction <- function(starting_airport,ending_airport,startingTime,threshold
   return(finalAnswer)
 }
 
+airport_data[,"arrive_depart"] <- "depart"
 
 RDU <- read.csv("thesis2021//RDU.csv")
 write.csv(MIA,"thesis2021//MIA.csv")
@@ -563,6 +756,9 @@ RDU <- RDU[,-1]
 MIA <- cbind(MIA,airport = "MIA")
 airport_data <- rbind(RDU,MIA)
 write.csv(airport_data,"thesis2021//airport_data_karlee.csv")
+
+MIA_arrive <- combineData(25.7617,-80.1918,"arrive",1)
+
 airport <- c("RDU","MIA")
 lat <- c(35.8801,25.7617)
 lon <- c(-78.7880,-80.1918)
